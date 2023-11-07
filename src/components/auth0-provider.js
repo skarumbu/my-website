30aa9cb7c645e6c96c11c62ac7574dc17c561e1f
skarumbu <karumbunathan@gmail.com>
2023-11-07 14:32:11 -0800

    Switch to using envrionment variables over secret manager
    In it's current form, it's hard to develop locally using secret manager,
    so will do a deep dive later on to see what is the best long term
    strategy.

--- src/components/auth0-provider.js
+++ src/components/auth0-provider.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
 const MyAuth0Provider = ({ children }) => {
-  const client = new SecretsManagerClient({ region: "us-east-1" });
-
-  const domain  =client.send(new GetSecretValueCommand({"SecretId": "domain"}))["SecretString"];
-  const clientId = client.send(new GetSecretValueCommand({"SecretId": "clientId"}))["SecretString"];
-
-
-
   return (
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain={process.env.domain}
+      clientId={process.env.clientId}
       redirectUri={window.location.origin}
     >
       {children}
