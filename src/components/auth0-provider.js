88d9345a45519abe479d4c3aa56fdcaf635665c6
skarumbu <karumbunathan@gmail.com>
2023-11-07 14:47:03 -0800

    Update Environment Variable Names
    Environment names must start with REACT_APP: https://stackoverflow.com/questions/72451199/auth0-login-returns-undefined

--- src/components/auth0-provider.js
+++ src/components/auth0-provider.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const MyAuth0Provider = ({ children }) => {
+  const domain = process.env.REACT_APP_DOMAIN
+  const clientId = process.env.REACT_APP_CLIENT_ID
+  if (domain == undefined || clientId == undefined) {
+    console.log("Environment variables undefined");
+  }
+
   return (
     <Auth0Provider
-      domain={process.env.domain}
-      clientId={process.env.clientId}
+      domain={domain}
+      clientId={clientId}
       redirectUri={window.location.origin}
     >
       {children}
