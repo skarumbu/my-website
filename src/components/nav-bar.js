92fa1eb72c8b41bc635aa68397b7745b75c7a831
skarumbu <karumbunathan@gmail.com>
2023-11-10 18:39:10 -0800

    Add Logout button
    Adding the code for the logout button. I'm just putting on the home
    screen now so it's visible somewhere, but eventually this should only be
    visible when a user is logged in.

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import LoginButton from "./login-button";
 import MyAuth0Provider from "./auth0-provider";
+import LogoutButton from "./logout-button";
 
 const NavBar = () => {
   return (
@@ -9,6 +10,7 @@ const NavBar = () => {
       <nav className="navbar" style = {{backgroundColor: "#282c34"}}>
           <MyAuth0Provider>
             <LoginButton />
+            <LogoutButton />
           </MyAuth0Provider>
       </nav>
     </div>
