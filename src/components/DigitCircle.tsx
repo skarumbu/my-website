8c0cb32c2eec5284d1a65361fdd5f72dd631ba3d
skarumbu <karumbunathan@gmail.com>
2024-10-09 21:13:07 -0700

    Make circles stay in their current spot

--- src/components/DigitCircle.tsx
+++ src/components/DigitCircle.tsx
@@ -10,10 +10,8 @@ interface DigitCircleProps {
 }
 
 const DigitCircle: React.FC<DigitCircleProps> = ({ id, value, shown, selected, onClick }) => {
-  if (!shown) return null;
-
   return (
-    <a className="Circle" onClick={() => onClick(id)} style={selected ? { backgroundColor: '#51a594' } : { backgroundColor: '#1f7a6e' }}>
+    <a className={`Circle${shown ? '' : '-Hidden'}`} onClick={() => onClick(id)} style={selected ? { backgroundColor: '#51a594' } : { backgroundColor: '#1f7a6e' }}>
       {value}
     </a>
   );
